b76e3e4ef1b98f850f8dc24d294f3ed228756428
mandip-kamaliya <144941685+mandip-kamaliya@users.noreply.github.com>
2025-12-18 20:22:16 +0530

    contract counterV2 added

--- src/CounterV2.sol
+++ src/CounterV2.sol
@@ -1,11 +1,13 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.18;
-
-contract CounterV1 {
+contract CounterV2 {
     uint256 public count;
 
     function increment() external {
         count += 1;
+    }
+
+    function decrememnt() external {
+        count -= 1;
     }
 }
