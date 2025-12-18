a43823a48d47825e542c7ad0584cfb68a415dafe
mandip-kamaliya <144941685+mandip-kamaliya@users.noreply.github.com>
2025-12-18 20:17:16 +0530

    added fallback function

--- src/proxy-contract.sol
+++ src/proxy-contract.sol
@@ -21,4 +21,20 @@ contract proxyContract {
     function upgrade(address _implementaition) external onlyAdmin {
         imlementaition = _implementaition;
     }
+
+    fallback() external payable {
+        address impl = implementation;
+
+        assembly {
+            calldatacopy(0, 0, calldatasize())
+
+            let result := delegatecall(gas(), impl, 0, calldatasize(), 0, 0)
+
+            returndatacopy(0, 0, returndatasize())
+
+            switch result
+            case 0 { revert(0, returndatasize()) }
+            default { return(0, returndatasize()) }
+        }
+    }
 }
