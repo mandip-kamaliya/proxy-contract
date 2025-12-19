6ac1f36d0eab925a63d392688374ece4babecc79
mandip-kamaliya <144941685+mandip-kamaliya@users.noreply.github.com>
2025-12-19 12:50:52 +0530

    added upgrade script file

---
+++ script/Upgrade.s.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import "../src/CounterV2.sol";
+import "../src/ProxyContract.sol";
+
+contract UpgradeScript is Script {
+    function run() external {
+        address PROXY_ADDRESS = vm.envAddress("PROXY");
+
+        vm.startBroadcast();
+
+        CounterV2 v2 = new CounterV2();
+        LearningProxy proxy = LearningProxy(PROXY_ADDRESS);
+
+        proxy.upgrade(address(v2));
+
+        console.log("Upgraded to CounterV2:", address(v2));
+
+        vm.stopBroadcast();
+    }
+}
