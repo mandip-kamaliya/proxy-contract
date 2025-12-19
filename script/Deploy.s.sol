117791fe2ebf4a42a115ab65a5f077e577788a2c
mandip-kamaliya <144941685+mandip-kamaliya@users.noreply.github.com>
2025-12-19 12:45:03 +0530

    fix:ci/cd error

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -6,16 +6,14 @@ import "forge-std/Script.sol";
 import "../src/CounterV1.sol";
 import "../src/ProxyContract.sol";
 
-contract DeployScript is Script{
-
-    function run() external{
+contract DeployScript is Script {
+    function run() external {
         vm.startBroadcast();
         CounterV1 V1 = new CounterV1();
         ProxyContract proxy = new ProxyContract(address(V1));
 
-        console.log("counterV1:",address(v1));
-        console.log("proxy:",address(proxy));
+        console.log("counterV1:", address(v1));
+        console.log("proxy:", address(proxy));
         vm.stopBroadcast();
     }
-
 }
