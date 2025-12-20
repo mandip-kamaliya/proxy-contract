bb9ddf5216f0ca05b1e5d07a1f3bbff74f4eee9a
mandip-kamaliya <144941685+mandip-kamaliya@users.noreply.github.com>
2025-12-20 19:29:35 +0530

    added setUp function

--- test/ProxyTest.t.sol
+++ test/ProxyTest.t.sol
@@ -5,3 +5,12 @@ import "forge-std/Test.sol";
 import "../src/CounterV1.sol";
 import "../src/CounterV2.sol";
 import "../src/ProxyContract.sol";
+
+contract ProxyTest is Test {
+    ProxyContract proxy;
+
+     function setUp() public {
+        CounterV1 v1 = new CounterV1();
+        proxy = new ProxyContract(address(v1));
+    }
+}
